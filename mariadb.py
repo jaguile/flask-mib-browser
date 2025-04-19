4558d1535b8b1a68229295204f43d0464bbe4047
jaguile <jaguilera126@gmail.com>
2025-04-19 10:20:00 +0200

    control d'error timeout base de dades

--- mariadb.py
+++ mariadb.py
@@ -2,25 +2,30 @@ import mysql.connector
 
 def get_oid_select():
 
-    mydb = mysql.connector.connect(
-        host = "192.168.56.101",
-        user = "mib",
-        password = "password",
-        database = "net_snmp"
-    )
-
-    cursorObject = mydb.cursor()
-
-    query = ("SELECT oid, traduccio from oids")
-
-    cursorObject.execute(query)
-
-    option_tuple = tuple(
-            {"oid": oid, "traduccio": traduccio} for oid, traduccio in cursorObject
+    try:
+        mydb = mysql.connector.connect(
+            host = "192.168.56.101",
+            user = "mib",
+            password = "password",
+            database = "net_snmp"
         )
 
-    cursorObject.close()
-    mydb.close()
-    return option_tuple
+        cursorObject = mydb.cursor()
 
+        query = ("SELECT oid, traduccio from oids")
+
+        cursorObject.execute(query)
+
+        option_tuple = tuple(
+                {"oid": oid, "traduccio": traduccio} for oid, traduccio in cursorObject
+            )
+
+        cursorObject.close()
+        mydb.close()
+        return option_tuple
+    
+    except mysql.connector.errors.DatabaseError:
+        print(f'No podem connectar a base de dades')
+        return None
+    
 # get_oid_select()
